43592be662175826187222c9799ecc989d1ae228
Flippie.Coetser <flippie.coetser@gmail.com>
2023-05-29 09:32:23 -0700

    CODE RUB: Add Example JSONLoader In Demo

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,7 @@
 import { Calculator } from "../lib/index.js";
 console.log(`Import module: Calculator`);
 console.log(`Use module: 1 + 2 = ${Calculator.Add(1, 2)}`);
+
+import { JSONLoader } from "../JSONLoader.js";
+let json = await JSONLoader.load("../test.json");
+console.log(json);

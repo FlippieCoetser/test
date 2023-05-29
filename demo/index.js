03c7d5743654c80927d48d243598b1d7b7b772e3
Flippie.Coetser <flippie.coetser@gmail.com>
2023-05-29 07:34:11 -0700

    CODE RUB: Update Lib and Demo

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,3 @@
-import { Calculator } from "calculator";
+import { Calculator } from "../lib/index.js";
 console.log(`Import module: Calculator`);
 console.log(`Use module: 1 + 2 = ${Calculator.Add(1, 2)}`);
